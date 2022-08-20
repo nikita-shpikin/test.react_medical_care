5ce6b4a097f0b2938473f046bbc7caa0a2043e9d
Nikita Shpikin <nikita.shpikin@gmail.com>
2022-08-20 18:19:29 +0500

    сверстал блок main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import style from './main.module.css';
+import heart from '../../assets/heart.svg';
+import notebook from '../../assets/notebook.svg';
+import stetoskop from '../../assets/stetoskop.svg';
 
 const Main = () => {
   return (
-    <div>
-      <h1>Main</h1>
+    <div className={style.main}>
+      <h1 className={style.title}>Место для получения медицинской помощи</h1>
+      <div className={style.wrapper}>
+        <button className={style.button}>Войти</button>
+        <button className={style.button}>Контакты</button>
+      </div>
+      <div className={style.cards}>
+        <div className={style.card}>
+          <img src={heart} alt="heart" />
+          <h2 className={style.cardTitle}>Экстренный Случай</h2>
+          <p className={style.text}>Рыба текст</p>
+        </div>
+        <div className={style.card}>
+          <img src={stetoskop} alt="stetoskop" />
+          <h2 className={style.cardTitle}>Экстренный Случай</h2>
+          <p className={style.text}>Рыба текст</p>
+        </div>
+        <div className={style.card}>
+          <img src={notebook} alt="notebook" />
+          <h2 className={style.cardTitle}>Лечение рака</h2>
+          <p className={style.text}>Рыба текст</p>
+        </div>
+      </div>
     </div>
   );
 }
