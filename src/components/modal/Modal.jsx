d39190c7ba3d48f1cc43b2f41125e590749fa6ac
Nikita Shpikin <nikita.shpikin@gmail.com>
2022-08-24 23:36:40 +0500

    реализовал валидацию авторизации

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -7,14 +7,31 @@ const Modal = ({ openModal, setOpenModal }) => {
   const { isAuth, setIsAuth } = useContext(AuthorizationContext);
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
+  const [loginValid, setLoginValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [loginValue, setLoginValue] = useState('Длина логина должна быть больше 8 символов');
+  const [passwordValue, setPasswordValue] = useState('Длина пароля должна быть больше 6 символов');
 
   const submit = () => {
     if (login === json.users[0].login && password === json.users[0].password) {
       setIsAuth(true);
       localStorage.setItem('auth', 'true');
       setOpenModal(false);
+      setPasswordValid(false);
+      setLoginValid(false);
+    } else if (login.length < 8 && password.length < 6) {
+      setLoginValid(true);
+      setPasswordValid(true);
+    } else if (login.length < 8) {
+      setLoginValid(true);
+    } else if (password.length < 6) {
+      setPasswordValid(true);
+    } else if (login !== json.users[0].login) {
+      setLoginValue('Не правильный логин');
+      setLoginValid(true);
     } else {
-      alert('нет!');
+      setPasswordValue('Не правильный пароль');
+      setPasswordValid(true);
     }
   }
 
@@ -24,12 +41,14 @@ const Modal = ({ openModal, setOpenModal }) => {
         className={style.mocalContent}
         onClick={e => e.stopPropagation()}
       >
+        {loginValid && <div style={{ color: 'red' }}>{loginValue}</div>}
         <input
           type="text"
           placeholder='логин'
           className={style.input}
           onChange={e => setLogin(e.target.value)}
         />
+        {passwordValid && <div style={{ color: 'red' }}>{passwordValue}</div>}
         <input
           type="password"
           placeholder='пароль'
